97f48e749e9a2c28cad0c6b972bb46efed3d9a3d
Brian <briankrause@gmail.com>
2026-02-16 15:43:35 -0600

    identify add new address buttons

--- playwright_automation/new_customer.py
+++ playwright_automation/new_customer.py
@@ -22,8 +22,9 @@ def add_address_on_detail_page(page: Page, customer: dict) -> None:
 
 
     # Open address dialog
-    page.get_by_role("button", name="Add New Address").click()
-    page.get_by_role("button", name="Add New Address").click()
+    page.locator("c-cmt-no-info-available").get_by_role("button", name="Add New Address").click()
+    #page.get_by_role("button", name="Add New Address").click()
+    #page.get_by_role("button", name="Add New Address").click()
     page.wait_for_timeout(2500)
 
     # Fill address fields (IDs from your known-working script)
